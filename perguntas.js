00822b1c1cbe10f22dfe563b33ec99b0a23872b8
heloisa simões <heloisasimoes9@gmail.com>
2023-04-25 20:30:18 -0300

    jogo de perguntas e respostas com javascript

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+export default [{
+        question: "Quanto é 80 + 60?",
+        answers: [
+            { option: "140", correct: true },
+            { option: "200", correct: false },
+            { option: "136", correct: false },
+        ],
+    },
+    {
+        question: "Quanto é 9 x 9?",
+        answers: [
+            { option: "66", correct: false },
+            { option: "81", correct: true },
+            { option: "11", correct: false },
+        ],
+    },
+    {
+        question: "Quanto é 10 x 8?",
+        answers: [
+            { option: "800", correct: false },
+            { option: "90", correct: false },
+            { option: "80", correct: true },
+        ],
+    },
+    {
+        question: "Quanto é 7 x 7?",
+        answers: [
+            { option: "70", correct: false },
+            { option: "56", correct: false },
+            { option: "49", correct: true },
+        ],
+    },
+];
